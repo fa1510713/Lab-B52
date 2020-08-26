e5461514117bf24345f766af90cf40b96fa28103
fa1510713 <54886284+fa1510713@users.noreply.github.com>
2020-08-26 16:01:53 +0300

    downloaded

---
+++ Lab 2 - Kotlin Fundamentals I/Lab2ASS/app/src/main/java/com/cmps312/lab2ass/JavaToKotlin.kt
@@ -0,0 +1,10 @@
+package com.cmps312.lab2ass
+
+fun main(args: Array<String>) {
+    print("Hello world")
+
+    var names : List<String> = ListOf("Fatema", "Abeer")
+    names.forEach{name -> println(name)}
+
+
+}
