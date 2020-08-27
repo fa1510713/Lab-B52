d4a0dfaaf1a00adf4cab83ed254566ba9182bd38
abdalabaaji <abdala.baaji@gmail.com>
2020-08-27 15:22:13 +0300

    lab 2

--- Lab 2 - Kotlin Fundamentals I/Tutorial/app/src/main/java/com/cmps312/lab2_kotlinfundamentals/Q2.kt
+++ Lab 2 - Kotlin Fundamentals I/Tutorial/app/src/main/java/com/cmps312/lab2_kotlinfundamentals/Q2.kt
@@ -1,7 +1,12 @@
 package com.cmps312.lab2_kotlinfundamentals
 
 fun main() {
-    for (i in 1..100){
-        print(i)
-    }
+    var x = readLine().toString().toInt()
+    var y = readLine().toString().toInt()
+    var z = readLine().toString().toInt()
+
+    var mult =  x * y *z
+
+    println("Mul =$mult")
+
 }
